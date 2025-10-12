be7eb91539d589f4e45e1641cbcdd7fb6daf12a0
Adarshkumar61 <adarshb2k61@gmail.com>
2025-10-12 17:57:45 +0530

    ex of multithreading

---
+++ MultiThreading_ex.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+def frame():
+    while True:
+        print('Frame capturing...🤖')
+        time.sleep(1)
+        
+def motor():
+    while True:
+        print('Motor is running...🚗')
+        time.sleep(0.5)
+
+t1 = threading.Thread(target=frame, daemon=True)
+t2 = threading.Thread(target=motor, daemon=True)
+t1.start()
+t2.start()
+input('Press Enter to stop...\n')
+print('Stopping threads and exiting.')
